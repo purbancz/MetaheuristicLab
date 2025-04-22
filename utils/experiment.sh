625f4cdbb0581e4d964e7158233b856122cbba48
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2025-04-22 22:42:38 +0200

    Add DCS-PSO

--- utils/experiment.sh
+++ utils/experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=PSO_hybrid_100dim
+#SBATCH --job-name=PSO_hybrid_100dim_bonus_benchmarks
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 #SBATCH --time=72:00:00
