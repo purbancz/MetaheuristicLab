4f97a1955407c034c816fabfe6b193df4f5b5161
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2025-07-26 15:25:52 +0200

    Change experimentation to an experimentation array

--- utils/experiment_array.sh
+++ utils/experiment_array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=drifter_algorithms_all_problems_1000dim
+#SBATCH --job-name=CMAES_100dim
 #SBATCH --output=slurm_logs/%x_%j.out # Log directory
 #SBATCH --error=slurm_logs/%x_%j.err  # Log directory
 #SBATCH --time=12:00:00           # Adjust time as needed
