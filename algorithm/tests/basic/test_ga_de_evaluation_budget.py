e3d2d9a4ba8cb1fd5aed4999a33d05e10674247f
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2026-08-30 09:04:44 +0200

    FIx evaluation budget in GeneticAlgorithm and DifferentialEvolution

---
+++ algorithm/tests/basic/test_ga_de_evaluation_budget.py
@@ -0,0 +1,35 @@
+import pytest
+from jmetal.operator import PolynomialMutation, SBXCrossover
+from jmetal.util.termination_criterion import StoppingByEvaluations
+
+from algorithm.basic.custom_ga import GeneticAlgorithm
+from algorithm.basic.differential_evolution import DifferentialEvolution
+from algorithm.tests.counting_problem import CountingProblem
+
+_POP = 4
+_MAX_EVAL = 20
+
+
+def test_ga_reported_evaluations_match_actual_calls():
+    problem = CountingProblem()
+    ga = GeneticAlgorithm(
+        problem=problem,
+        population_size=_POP,
+        offspring_population_size=_POP,
+        crossover=SBXCrossover(1.0, 5.0),
+        mutation=PolynomialMutation(1.0 / problem.number_of_variables(), 20.0),
+        termination_criterion=StoppingByEvaluations(max_evaluations=_MAX_EVAL),
+    )
+    ga.run()
+    assert problem.evaluation_count == ga.evaluations
+
+
+def test_de_reported_evaluations_match_actual_calls():
+    problem = CountingProblem()
+    de = DifferentialEvolution(
+        problem=problem,
+        swarm_size=_POP,
+        termination_criterion=StoppingByEvaluations(max_evaluations=_MAX_EVAL),
+    )
+    de.run()
+    assert problem.evaluation_count == de.evaluations
